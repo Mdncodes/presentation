f1aae432185569a2fa80f96326db9890bb745625
Mdncodes <MadaniNiasse@outlook.de>
2019-11-10 14:19:18 +0100

    Updates

---
+++ precache-manifest.64e8c52dfd3ef0e7bbe79d171e4a5beb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800ad50719f62438c65c14b3fa25ea59",
+    "url": "/presentation/index.html"
+  },
+  {
+    "revision": "1301048f23789ff743da",
+    "url": "/presentation/static/css/main.c521b55f.chunk.css"
+  },
+  {
+    "revision": "68435ec5ba5dada47979",
+    "url": "/presentation/static/js/2.2d9877c9.chunk.js"
+  },
+  {
+    "revision": "1301048f23789ff743da",
+    "url": "/presentation/static/js/main.34d3df5a.chunk.js"
+  },
+  {
+    "revision": "20cd08e2bd0eed284f18",
+    "url": "/presentation/static/js/runtime-main.cb131830.js"
+  },
+  {
+    "revision": "a118b917cabc6704cb80b63f6b3b504e",
+    "url": "/presentation/static/media/mqtt.a118b917.png"
+  },
+  {
+    "revision": "a3233301e4d2daa9d79df9557c8237c4",
+    "url": "/presentation/static/media/mypic.a3233301.JPG"
+  },
+  {
+    "revision": "92e545060c71cd7c104c71797a0bd745",
+    "url": "/presentation/static/media/nodejsicon.92e54506.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/presentation/static/media/reacticon.82a318cc.png"
+  },
+  {
+    "revision": "304062605793ba07eed9e64bd032096b",
+    "url": "/presentation/static/media/tsicon.30406260.png"
+  }
+]);
